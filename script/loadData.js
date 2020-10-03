13204eac1922581f667b96fd289cccc5b9b5d162
Yulia <youlka25@yandex.ru>
2020-10-03 10:46:51 +0300

    вывод карточек товаров из каталога

--- script/loadData.js
+++ script/loadData.js
@@ -2,7 +2,6 @@ import {
     getData
 } from './getData.js';
 
-const wishlist = ['idd005', 'idd085', 'idd096'];
 
 const cartList = [{
         id: 'idd015',
@@ -19,19 +18,6 @@ const cartList = [{
 ];
 
 export const loadData = () => {
-    if (location.search) {
-        const search = decodeURI(location.search);
-        const prop = search.split('=')[0].slice(1);
-        const value = search.split('=')[1];
-
-        if (prop === 's') {
-            getData.search(value, (data) => console.log(data));
-        } else if (prop === 'wishlist') {
-            getData.wishlist(wishlist, (data) => console.log(data));
-        } else if (prop === 'cat' || prop === 'subcat') {
-            getData.category(prop, value, (data) => console.log(data));
-        }
-    }
 
     if (location.hash) {
         getData.item(location.hash.slice(1), (data) => console.log(data));
